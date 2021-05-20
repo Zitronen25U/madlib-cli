1d5332dfb110f69f2010ff4e34197537118a375b
Brian Lemons <brian_lemons@hotmail.com>
2021-05-19 23:35:12 -0700

    setup

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,10 @@
+
+
+def greeting():
+  name = input("Enter Your Name >")
+  print (f"""Welcome to madlibs, {name}
+The rules of the game are simple:
+Fill in the blanks with the corresponding prompts
+  """)
+  
+greeting()
